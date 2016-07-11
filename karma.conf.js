5c73a8de96b14e34c733d86a0327ee10c7b18938
Michael Bareja <michal.bareja@pragmatists.pl>
2016-07-11 14:34:52 +0200

    configured for es6

--- karma.conf.js
+++ karma.conf.js
@@ -15,8 +15,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'src/*.js',
-      'spec/*.js'
+      'src/**/*.js',
+      'spec/**/*.js'
     ],
 
 
@@ -27,7 +27,9 @@ module.exports = function(config) {
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
+    preprocessors : {
+      'src/**/*.js' : ['babel'],
+      'spec/**/*.js' : ['babel']
     },
 
 
